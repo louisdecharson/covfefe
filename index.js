8a0ab593850b9812a09e3c4ce78db6dae36c70fe
Louis de Charson <louisdecharson@posteo.net>
2018-11-23 14:10:02 +0000

    add option to retrieve the xth tweet instead of the last one

--- index.js
+++ index.js
@@ -3,16 +3,28 @@
 const donaldURL = "https://twitter.com/realDonaldTrump/";
 // PACKAGES
 var request = require('request'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    program = require('commander');
 
-request(donaldURL, function(err,res,html) {
-    if (!err && res.statusCode == 200) {
-        var $ = cheerio.load(html);
-        var donaldTweet = $('p.tweet-text').eq(0).text();
-        var donaldTime = $('a.tweet-timestamp').eq(0).children().eq(0).text();
-        donaldTweet = '@realDonaldTrump ('+donaldTime+' ago)'+': "' + donaldTweet + '"';
-        console.log(donaldTweet);
-    } else {
-        console.log('No internet connection');
-    }
-});
+program
+    .option('-t, --tweet <tweet>','X th tweet to retrieve. Default is the last one. -t 1 will retrieve the penultimate.')
+    .action(function() {
+        if (program.tweet) {
+            donaldTweetNb = program.tweet;
+        } else {
+            donaldTweetNb = 0;
+        }
+        request(donaldURL, function(err,res,html) {
+            if (!err && res.statusCode == 200) {
+                var $ = cheerio.load(html);
+                var donaldTweet = $('p.tweet-text').eq(donaldTweetNb).text();
+                var donaldTime = $('a.tweet-timestamp').eq(donaldTweetNb).children().eq(0).text();
+                donaldTweet = '@realDonaldTrump ('+donaldTime+' ago)'+': "' + donaldTweet + '"';
+                console.log(donaldTweet);
+            } else {
+                console.log('No internet connection');
+            }
+        });
+    })
+    .parse(process.argv);
+
